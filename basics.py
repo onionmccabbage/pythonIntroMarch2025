6c5f74034030d74dc5af21b0f9e7f607271720be
onionmccabbage <onionmccabbage@live.com>
2025-03-10 10:15:37 +0000

    basics

---
+++ basics.py
@@ -0,0 +1,8 @@
+# here is a comment
+# Python files are known as modules
+# they are just plain text files
+
+a = 3   # this is an integer
+b = 8.7 # this is a float
+
+print( a, type(a), b, type(b) )
