b4a95d804721294f112ee33e7875b04b4059a7c8
onionmccabbage <onionmccabbage@live.com>
2025-03-10 16:03:46 +0000

    importing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # often we call our module 'main.py'
-import utility # you can define a path the the module
+# import utility # you can define a path the the module
 import my_tools
+from utility import makeInt
+
 
 # we can now use the code from other modules
-i = utility.makeInt(3.21)
+i = makeInt(3.21)
 print(i) # 3
+
+l = 'this is my text'
+u = my_tools.convertUpper(l)
+print(u)
