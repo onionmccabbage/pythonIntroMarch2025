e982bfef601ca1a9d84629ea399668ed455f3cb0
onionmccabbage <onionmccabbage@live.com>
2025-03-10 16:32:18 +0000

    end of day 1

--- using_sys.py
+++ using_sys.py
@@ -4,3 +4,12 @@ import sys
 sys.path.append('d:/pythonIntroMarch2025/demo_stuff')
 
 print( sys.path )
+
+print(sys.version, sys.version_info)
+
+print( sys.maxsize ) # 2**64-1 (on a 64 bit machine)
+
+# however, Python will always look for a way to exceed limiations....
+biggy = 2**9024
+
+print(biggy)
