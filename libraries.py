303420cb812995cf501550c3072de1b80fadbaf4
onionmccabbage <onionmccabbage@live.com>
2025-03-10 13:24:41 +0000

    using random

--- libraries.py
+++ libraries.py
@@ -5,6 +5,11 @@ import random
 
 def makeRandNumber():
     '''We can generate a random number'''
-    r = random.randint() # return a random integer
+    r = random.randint(0,10) # return a random integer
+    # by default every functio will return anyway
+    # but in this case we probably want to return something
+    return r
 
-print( makeRandNumber() )
+# sometimes we need to call a function repeatedly
+for i in range(0,10): # range givves us values between (start, stop-before)
+    print( makeRandNumber() )
