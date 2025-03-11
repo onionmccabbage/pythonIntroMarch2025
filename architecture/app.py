e1c3b3bc0b1a4bd0c71d9e82f8d4325558dec7c8
onionmccabbage <onionmccabbage@live.com>
2025-03-11 15:55:22 +0000

    working with imports and strings

--- architecture/app.py
+++ architecture/app.py
@@ -1,5 +1,14 @@
 # combine every member of a collection into a single string
+from stuff import checkTuple
+
 
 def combine(t):
     '''validate we have a tuple, 
     then combine each member of the tuple into a string'''
+    values = checkTuple(t)
+    s = '-'.join( str(t) )
+    return s
+
+if __name__ == '__main__':
+    t = (True, None, 'all', 'is', 'good')
+    print(combine(t))
