5846fa7e5d36d67ca7b0d2fd0bf34928a9d42186
onionmccabbage <onionmccabbage@live.com>
2025-03-11 16:19:08 +0000

    using set data-type

--- unpacking.py
+++ unpacking.py
@@ -3,7 +3,9 @@ def doUnpack(struct):
     name, age, creation = struct
     return f'We have {name}, {age} and {creation}'
 
-
+def packIntoStruct(a,b,c,d,e):
+    s = [a,b,c,d,e] # we have a list
+    return s
 
 if __name__ == '__main__':
     # here is a structure
@@ -14,3 +16,11 @@ if __name__ == '__main__':
     l = ['Floella', 68, 'TV']
     r = doUnpack(l)
     print(r)
+    # we can collect separate things together into a single structure
+    m='Coffee'
+    n='Tea'
+    p='Orange'
+    q='Water'
+    w='Other'
+    z = packIntoStruct(m, n, p, q, w)
+    print(z, type(z))
---
+++ using_set.py
@@ -0,0 +1,8 @@
+my_d = {'name':'Deidre', 'level':'admin'} # not ordinal
+# as well as list, tuple and dict ther is also a 'set' data type
+# a set is a mutable non ordinal collection of UNIQUE values of any data type
+my_set = {5,4,5,8,6,7,8,'any data type', None}
+my_set.add(66) # the set is always mutable
+my_set.remove(5)
+print(my_set, type(my_set))
+
