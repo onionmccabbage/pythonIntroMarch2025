df6ca9fcfadb48b2782c583f7d34dd775248af41
onionmccabbage <onionmccabbage@live.com>
2025-03-11 11:40:13 +0000

    write to a text file

--- fifo/write_to_file.py
+++ fifo/write_to_file.py
@@ -2,9 +2,9 @@ def writeOutput(t):
     '''write the text 't' to a text file'''
     # we need a file access object
     fout = open('my_text.txt', 'at')
-    fout.write(t)
+    fout.write(f'{t}\n') # here we add a new line character
     fout.close()# always a good idea to tidy up
 
 # exercise the code
-t = 'more conent written to file'
+t = 'more content written to file'
 writeOutput(t)
