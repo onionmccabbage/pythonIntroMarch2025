83fec2300313ede0bcbced34aa5c9c7ceb7e4372
onionmccabbage <onionmccabbage@live.com>
2025-03-11 11:50:21 +0000

    retrieve just one line of text from a file

--- fifo/read_from_file.py
+++ fifo/read_from_file.py
@@ -1,10 +1,19 @@
 def readTextFile():
     '''retrieve the contents of a text file'''
-    fin = open('my_file.txt', 'rt') # 'r' will read 't' specifies text
+    fin = open('my_text.txt', 'rt') # 'r' will read 't' specifies text
     t = fin.read() # retrieve the entire contents of the text file into memory
     fin.close()
+    return t
+
+# we may also read just a part of the file
+def readSomeText():
+    '''Retreive just a part of the text file'''
+    fin = open('my_text.txt', 'rt')
+    t = fin.readline() # just retrieve one line
     return t
 
 # exercise the code
 retrieved = readTextFile()
 print(retrieved, type(retrieved))
+line = readSomeText()
+print(line)
