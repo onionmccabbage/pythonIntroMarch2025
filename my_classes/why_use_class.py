65743c2a3d01c2fe9e87f2d8c4cac6cdb75407eb
onionmccabbage <onionmccabbage@live.com>
2025-03-11 16:30:06 +0000

    simple example of a python class

---
+++ my_classes/why_use_class.py
@@ -0,0 +1,22 @@
+# we already have plenty of stuctures to collect related data into (list, tuple, dict...)
+# The problem is none of the built in stutures have any means to validate
+
+# suppose we need to guarantee the 'name' is a string, and the 'age' is a positive integer
+# we need to write validation code
+# classes let us build the validation into the data structure
+
+p1 = ('Sharon', 54)
+p2 = ['Ethel', 33]
+p3 = {'Oenid', 67}
+p4 = {True, -55} # this is not suitable for a person
+
+# here is the most basic Python class
+class Person:
+    # we usually include an __init__ method in a class
+    def __init__(self): # this function gets caled every time we use this class to make an instance
+        pass
+
+if __name__ == '__main__':
+    # we can make instances of our 'Person' class
+    pA = Person()
+    pB = Person()
--- using_set.py
+++ using_set.py
@@ -1,5 +1,5 @@
 my_d = {'name':'Deidre', 'level':'admin'} # not ordinal
-# as well as list, tuple and dict ther is also a 'set' data type
+# as well as list, tuple and dict there is also a 'set' data type
 # a set is a mutable non ordinal collection of UNIQUE values of any data type
 my_set = {5,4,5,8,6,7,8,'any data type', None}
 my_set.add(66) # the set is always mutable
