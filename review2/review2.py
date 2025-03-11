e727e3fbc15306438796004b2de5af8e0e14f052
onionmccabbage <onionmccabbage@live.com>
2025-03-11 15:26:17 +0000

    sample solution for review exercise 2

--- review2/review2.py
+++ review2/review2.py
@@ -1,6 +1,6 @@
 import requests
 from util.sanitize import cleanup
-from fifo.write_text import writeToFile
+from write_to_file import writeOutput
 
 def get_data(category, id):
     full_path = f'https://jsonplaceholder.typicode.com/{category}/{id}'
@@ -16,15 +16,16 @@ def main():
     cleaned = cleanup(category=which_cat, id=which_id)
     # make a request and return the json
     data = get_data(category=cleaned['category'], id=cleaned['id'])
-    resultString = f'Category {cleaned["category"]} member {cleaned['id']} gives the following:\n'
+    # NB be careful - here we have double quotes inside single quotes (which is fine)
+    resultString = f'Category {cleaned["category"]} member {cleaned["id"]} gives the following:\n'
     for k, v in data.items():
         print(f'\t{k}: {v}')
         resultString += f'\t{k}: {v}\n'
     print(resultString)
-    writeToFile(resultString)
+    writeOutput(resultString)
     # if category is 'users' then we also want all the posts
     if cleaned['category'] == 'users':
-        print(f'Posts for user {cleaned['id']}:')
+        print(f'Posts for user {cleaned["id"]}:')
         posts = get_data(category='posts', id='')
         for item in posts: # posts is a list
             if item['userId']==cleaned['id']:
