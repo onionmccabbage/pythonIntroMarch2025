77b0c57ae6f483c48e8cff3b301462044300e78c
onionmccabbage <onionmccabbage@live.com>
2025-03-11 10:08:39 +0000

    range and generators

--- using_range.py
+++ using_range.py
@@ -13,3 +13,14 @@ odd_numbers = range(1,10,2) # we get 1, 3, 5, 7, 9
 # we can use a range to generate calculated values
 square_numbers = [i*i for i in range(0,10)] # 0, 1, 4, 9, 16, 25...
 print(square_numbers)
+
+# Python includes a 'generator' object
+# suppose we need a sequence of integers, from zero to 10^1000
+g = (i for i in range(0, 1000)) # this is a generator
+print(type(g))
+# we may access the next member of the generator by iterating
+for i in g:
+    print(i)
+
+# what about tuple...
+t = (range(0,10),True)
