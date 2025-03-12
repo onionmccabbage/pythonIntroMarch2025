e60b2549f76506bc58e0b7b9b5234b313db15095
onionmccabbage <onionmccabbage@live.com>
2025-03-12 10:15:15 +0000

    a simple useful class

--- my_classes/class_a.py
+++ my_classes/class_a.py
@@ -2,6 +2,7 @@
 class Device:
     '''Construct a device which knows about temperature and baud rate'''
     # every function that belongs to a class MUST take 'self' as the first argument
+    # In Python there is no public, private, protected....
     def __init__(self, t, b): # this is 'dunder' init
         '''temperature and baud should be numeric values'''
         if type(t) in (int, float):
@@ -17,6 +18,12 @@ if __name__ == '__main__':
     # we can create instances of the Device
     d1 = Device(12, 1024)
     d2 = Device(27, 2048)
-    d3 = Device(None, 'oops') # this gets validated
+    try:
+        d3 = Device(None, 'oops') # this gets validated
+    except TypeError as te:
+        print(f'Problem: {te}')
 
-    print(d1, d2)
+    # we may mutate the properties of the class instances
+    d1.baud = 2048 # we may access instance properties using dot-syntax
+
+    print(d1.baud, d2)
