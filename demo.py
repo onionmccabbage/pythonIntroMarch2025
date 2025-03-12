aa683d8e1315587283b562d972cea79e38ef94c2
onionmccabbage <onionmccabbage@live.com>
2025-03-12 13:09:47 +0000

    working with data types

--- demo.py
+++ demo.py
@@ -12,3 +12,10 @@ def getData():
 
 def util():
     pass
+
+# data types
+t = (1,2,3)
+l = list(t) # we make our tuple into a list
+
+c = Exception()
+
--- my_classes/class_c.py
+++ my_classes/class_c.py
@@ -2,6 +2,7 @@ from class_b import Point
 
 # any class may inherit from any other class
 class SpacePoint(Point):
+    # NB be docstring MUST be the first line of code in any structure
     '''as well as x and y (from Point) we also need a numeric 'z' value'''
     __slots__ = ['__x', '__y', '__z']
     def __init__(self, x, y, z):
@@ -18,6 +19,10 @@ class SpacePoint(Point):
             self.__z = 1
     def __str__(self):
         return f'This 3-d point is at {self.x}, {self.y}, {self.z}'
+    # we may choose to add functionality to a class - often to derive values
+    def deriveHypot(self):
+        '''use the values of x and y to return the hypotenuse'''
+        return (self.x*self.x+self.y*self.y)**0.5
 
 if __name__ == '__main__':
     # we may createt instances of our class
@@ -25,3 +30,4 @@ if __name__ == '__main__':
     SpacePoint.__doc__ = 'changed'
     print(s1) # this calls __str__
     print(SpacePoint.__doc__) # everything can access documentation string via __doc__
+    print(s1.deriveHypot()) # 5.0
